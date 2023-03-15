9679612f731495bd378005c8b60bfa10e0aca52e
feh-franc0 <fernandofv1110@gmail.com>
2023-03-15 03:48:17 -0300

    Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Fernando Franco',
+    email: 'fernando@prisma.com.br',
+  },
+})
+
+// ORM - Object Relational Mapper
