6f83446fcf4d5b075a74c66f4afe74841068ceee
feh-franc0 <fernandofv1110@gmail.com>
2023-03-15 00:14:37 -0300

    Carregando variáveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0', // acessivel para fronts
-  port: 3333,
+  port: env.PORT,
 }).then(() => {
   console.log('🏃‍♀️ HTTP Server Running!')
 })
