ea3cdd0c66c11eaa736e6a4e903944ef78553bed
feh-franc0 <fernandofv1110@gmail.com>
2023-03-18 07:17:20 -0300

    Handler de erros global

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,7 +3,6 @@ import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
-// import { InMemoryUsersRepostory } from '@/repositories/in-memory-users-reposiroty'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -28,7 +27,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       return reply.status(409).send({ message: err.message })
     }
 
-    return reply.status(500).send() // TODO: fix me
+    throw err
   }
 
   return reply.status(201).send()
